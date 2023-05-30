03f313864920b660348091c09850c6bc52831ebc
lauti7 <lautigbustos@gmail.com>
2023-05-30 12:45:57 -0300

    feat: ws + signed headers

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,62 @@
 pub mod signed_fetch;
 #[cfg(feature = "ws")]
 pub mod ws;
+#[cfg(feature = "ws_signed_headers")]
+pub mod ws_signed_headers;
 
 #[cfg(test)]
-fn create_test_identity() -> dcl_crypto::Identity {
-    dcl_crypto::Identity::from_json(
-        r#"{
-       "ephemeralIdentity": {
-         "address": "0x84452bbFA4ca14B7828e2F3BBd106A2bD495CD34",
-         "publicKey": "0x0420c548d960b06dac035d1daf826472eded46b8b9d123294f1199c56fa235c89f2515158b1e3be0874bfb15b42d1551db8c276787a654d0b8d7b4d4356e70fe42",
-         "privateKey": "0xbc453a92d9baeb3d10294cbc1d48ef6738f718fd31b4eb8085efe7b311299399"
-       },
-       "expiration": "3021-10-16T22:32:29.626Z",
-       "authChain": [
-         {
-           "type": "SIGNER",
-           "payload": "0x7949f9f239d1a0816ce5eb364a1f588ae9cc1bf5",
-           "signature": ""
+mod test_utils {
+    pub enum ReplyType {
+        InvalidMessageError,
+        InvalidSignatureError,
+        TimeoutError,
+        AuthChain,
+    }
+
+    // Mock for simulating a connection
+    pub struct InMemory {
+        pub sender: tokio::sync::mpsc::Sender<String>,
+        pub receiver: tokio::sync::mpsc::Receiver<String>,
+        pub reply_type: ReplyType,
+    }
+
+    impl InMemory {
+        pub fn new(reply_type: ReplyType) -> Self {
+            let chann = tokio::sync::mpsc::channel(1);
+            Self {
+                sender: chann.0,
+                receiver: chann.1,
+                reply_type,
+            }
+        }
+
+        pub async fn send_challenge(&self, auth_chain: &str) {
+            self.sender.send(auth_chain.to_string()).await.unwrap();
+        }
+    }
+
+    pub fn create_test_identity() -> dcl_crypto::Identity {
+        dcl_crypto::Identity::from_json(
+          r#"{
+         "ephemeralIdentity": {
+           "address": "0x84452bbFA4ca14B7828e2F3BBd106A2bD495CD34",
+           "publicKey": "0x0420c548d960b06dac035d1daf826472eded46b8b9d123294f1199c56fa235c89f2515158b1e3be0874bfb15b42d1551db8c276787a654d0b8d7b4d4356e70fe42",
+           "privateKey": "0xbc453a92d9baeb3d10294cbc1d48ef6738f718fd31b4eb8085efe7b311299399"
          },
-         {
-           "type": "ECDSA_EPHEMERAL",
-           "payload": "Decentraland Login\nEphemeral address: 0x84452bbFA4ca14B7828e2F3BBd106A2bD495CD34\nExpiration: 3021-10-16T22:32:29.626Z",
-           "signature": "0x39dd4ddf131ad2435d56c81c994c4417daef5cf5998258027ef8a1401470876a1365a6b79810dc0c4a2e9352befb63a9e4701d67b38007d83ffc4cd2b7a38ad51b"
-         }
-       ]
-      }"#,
-    ).unwrap()
+         "expiration": "3021-10-16T22:32:29.626Z",
+         "authChain": [
+           {
+             "type": "SIGNER",
+             "payload": "0x7949f9f239d1a0816ce5eb364a1f588ae9cc1bf5",
+             "signature": ""
+           },
+           {
+             "type": "ECDSA_EPHEMERAL",
+             "payload": "Decentraland Login\nEphemeral address: 0x84452bbFA4ca14B7828e2F3BBd106A2bD495CD34\nExpiration: 3021-10-16T22:32:29.626Z",
+             "signature": "0x39dd4ddf131ad2435d56c81c994c4417daef5cf5998258027ef8a1401470876a1365a6b79810dc0c4a2e9352befb63a9e4701d67b38007d83ffc4cd2b7a38ad51b"
+           }
+         ]
+        }"#,
+      ).unwrap()
+    }
 }
---
+++ src/ws_signed_headers.rs
@@ -0,0 +1,179 @@
+use crate::signed_fetch::{self, AuthMiddlewareError, VerificationOptions};
+use dcl_crypto::{Address, Authenticator, Web3Transport};
+use serde_json::Value;
+use std::{collections::HashMap, time::Duration};
+
+#[derive(Debug)]
+pub enum WSAuthSignedHeadersError {
+    /// Error on the connection
+    ConnectionError,
+    /// The time elapsed without sending the signed headers
+    Timeout,
+    /// The messaged sent by the client is not a valid authchain
+    InvalidMessage,
+    /// Error produced by [`signed_fetch::verify`]
+    VerifyError(AuthMiddlewareError),
+}
+
+/// Trait that should be implemented by the type in charge of handling the websocket connection for [`authenticate_dcl_user_with_signed_headers`]
+#[async_trait::async_trait]
+pub trait AuthenticatedWSWithSignedHeaders {
+    type Error;
+    /// Receives the authchain with signed headers
+    async fn receive_signed_headers(&mut self) -> Result<String, Self::Error>;
+}
+
+pub async fn authenticate_dcl_user_with_signed_headers<
+    Ws: AuthenticatedWSWithSignedHeaders,
+    T: Web3Transport,
+>(
+    method: &str,
+    path: &str,
+    ws: &mut Ws,
+    timeout: u64,
+    authenticator: Authenticator<T>,
+) -> Result<Address, WSAuthSignedHeadersError> {
+    match tokio::time::timeout(Duration::from_secs(timeout), ws.receive_signed_headers()).await {
+        Ok(Ok(client_response)) => {
+            if let Ok(signed_headers) =
+                serde_json::from_str::<HashMap<String, Value>>(&client_response)
+            {
+                let signed_headers = signed_headers
+                    .iter()
+                    .map(|(key, value)| (key.to_owned(), value.to_string()))
+                    .collect::<HashMap<String, String>>();
+
+                signed_fetch::verify(
+                    method,
+                    path,
+                    signed_headers,
+                    VerificationOptions::with_authenticator(authenticator),
+                )
+                .await
+                .map_err(WSAuthSignedHeadersError::VerifyError)
+            } else {
+                Err(WSAuthSignedHeadersError::InvalidMessage)
+            }
+        }
+        Ok(Err(_)) => Err(WSAuthSignedHeadersError::ConnectionError),
+        Err(_) => Err(WSAuthSignedHeadersError::Timeout),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    use super::*;
+    use crate::test_utils::*;
+
+    #[async_trait::async_trait]
+    impl AuthenticatedWSWithSignedHeaders for InMemory {
+        type Error = String;
+
+        async fn receive_signed_headers(&mut self) -> Result<String, Self::Error> {
+            match self.receiver.recv().await {
+                Some(challenge) => match &self.reply_type {
+                    ReplyType::AuthChain => Ok(challenge),
+                    ReplyType::InvalidMessageError => Ok(String::from("")),
+                    ReplyType::TimeoutError => {
+                        tokio::time::sleep(Duration::from_secs(2)).await; // 1 second passed to the func
+                        Ok(String::from(""))
+                    }
+                    ReplyType::InvalidSignatureError => Ok("".to_string()),
+                },
+                None => Err("Error".to_string()),
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn authtentication_with_signed_headers_should_return_ok() {
+        let identity = create_test_identity();
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+
+        let chain = identity.sign_payload(format!("get:/:{}:{}", now, "{}"));
+
+        let signed_headers = format!(
+            r#"{{"X-Identity-Auth-Chain-0": {},  "X-Identity-Auth-Chain-1": {},  "X-Identity-Auth-Chain-2": {}, "X-Identity-Timestamp": {}, "X-Identity-Metadata": {} }}"#,
+            serde_json::to_string(chain.get(0).unwrap()).unwrap(),
+            serde_json::to_string(chain.get(1).unwrap()).unwrap(),
+            serde_json::to_string(chain.get(2).unwrap()).unwrap(),
+            now,
+            "{}"
+        );
+
+        let mut in_memory_connection = InMemory::new(ReplyType::AuthChain);
+        in_memory_connection.send_challenge(&signed_headers).await;
+
+        let address = authenticate_dcl_user_with_signed_headers(
+            "get",
+            "/",
+            &mut in_memory_connection,
+            1,
+            Authenticator::new(),
+        )
+        .await
+        .unwrap();
+
+        assert_eq!(
+            address.to_string(),
+            "0x7949f9f239d1a0816ce5eb364a1f588ae9cc1bf5"
+        )
+    }
+
+    #[tokio::test]
+    async fn authtentication_with_signed_headers_should_return_err() {
+        let identity = create_test_identity();
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+
+        let chain = identity.sign_payload(format!("post:/:{}:{}", now, "{}"));
+
+        let signed_headers = format!(
+            r#"{{"X-Identity-Auth-Chain-0": {},  "X-Identity-Auth-Chain-1": {},  "X-Identity-Auth-Chain-2": {}, "X-Identity-Timestamp": {}, "X-Identity-Metadata": {} }}"#,
+            serde_json::to_string(chain.get(0).unwrap()).unwrap(),
+            serde_json::to_string(chain.get(1).unwrap()).unwrap(),
+            serde_json::to_string(chain.get(2).unwrap()).unwrap(),
+            now,
+            "{}"
+        );
+
+        let mut in_memory_connection = InMemory::new(ReplyType::AuthChain);
+        in_memory_connection.send_challenge(&signed_headers).await;
+
+        assert!(matches!(
+            authenticate_dcl_user_with_signed_headers(
+                "get",
+                "/",
+                &mut in_memory_connection,
+                1,
+                Authenticator::new(),
+            )
+            .await
+            .unwrap_err(),
+            WSAuthSignedHeadersError::VerifyError(AuthMiddlewareError::Unauthotized)
+        ));
+
+        let mut in_memory_connection = InMemory::new(ReplyType::AuthChain);
+        in_memory_connection.send_challenge("").await;
+
+        assert!(matches!(
+            authenticate_dcl_user_with_signed_headers(
+                "get",
+                "/",
+                &mut in_memory_connection,
+                1,
+                Authenticator::new(),
+            )
+            .await
+            .unwrap_err(),
+            WSAuthSignedHeadersError::InvalidMessage
+        ))
+    }
+}
